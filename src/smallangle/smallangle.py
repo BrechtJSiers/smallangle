950ed4ea32324c2e871e6cbf1fd3773c05eed1e5
BrechtSrs <post@brechtjesiers.nl>
2024-11-14 11:36:00 +0100

    added clicks to run in terminal, added docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,18 +3,42 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass  
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+
+@click.option(
+    "-n",
+    default=6
+)
+
+def sin(n):
+    """ Gives the sin between 0 and 2pi.
+
+    Args:
+        n (integer): number of steps taken between 0 and 2pi.
+    """
+    x = np.linspace(0, 2 * pi, n)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
+@cmd_group.command()
 
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+@click.option(
+    "-n",
+    default=6
+)
+def tan(n):
+    """Gives the tan between 0 and 2pi
+
+    Args:
+        n (integer): number of steps taken between 0 and 2pi.
+    """
+    x = np.linspace(0, 2 * pi, n)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
-
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
